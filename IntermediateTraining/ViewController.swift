b4d579e43b1f2d49c1fcef882bd32577faafd49f
Gin Imor <openheart23@163.com>
2021-01-30 09:40:20 +0800

    Initial Commit

---
+++ IntermediateTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntermediateTraining
+//
+//  Created by Gin Imor on 1/30/21.
+//  Copyright © 2021 Brevity. All rights reserved..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
